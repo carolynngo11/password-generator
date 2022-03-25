8f65a68fdb31454c05cc29252d88dd9d966d4501
Carolyn Ngo <carolynngo11@yahoo.com>
2022-03-24 21:11:58 -0500

    Added boolean characters

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,6 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
@@ -19,11 +18,23 @@ function generatePassword(){
 }
 
 // 1. Create variables to save user input from prompts passwordLength string (and it needs to be integer ultimately) specialCharacter boolean
+// specialCharacters boolean
+var specialCharacters = ['!', '"', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '_', '=', '`', '~', '[', ']', '{', '}', '|', ';', ':', ',', '.', '<', '>', '?', '/'];
+
 // numercCharacters boolean
+var numercCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
 // lowercaseCharacters boolean
-// uppercase Characters boolean
+var lowercaseCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'z'];
+
+// uppercaseCharacters boolean
+var uppercaseCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
   // alert, confirm, prompt
+
   // Use conditionals to validate inputs (password length, use enters no inputs)
+
+
 
 // 2. Create arrays for each type of character
   // specialCharacters, uppercase, lowercase, numeric
